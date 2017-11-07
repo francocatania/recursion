a9741a936c810a821e92f6e729cc8022580e870e
Franco <frc.catani@gmail.com>
2017-11-07 15:04:38 -0300

    StringifyJSON Complete

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,55 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  
+// BASE CASES.
+  if (typeof(obj) === 'number') {
+	return '' + obj;
+  }
+
+  if (obj === null) {
+  	return '' + obj;
+  }
+
+  if (typeof(obj) === 'boolean') {
+    return '' + obj;	
+  }
+
+  if (typeof(obj) === 'string') {
+  	return '"' + obj + '"';
+  }
+
+// Recursion Cases.
+  if (Array.isArray(obj)) {
+  	var stringifiedArray = '[';
+  	if (obj.length) {
+  	  for (var i = 0; i < obj.length; i++) {
+  	    if (i != obj.length - 1) {
+  		  stringifiedArray += (stringifyJSON(obj[i]) + ',');
+  	    } else {
+  		  stringifiedArray += (stringifyJSON(obj[i]) + ']');
+  	    }
+  	  }	
+  	} else {
+  	  stringifiedArray += ']';
+  	}
+  	return stringifiedArray;
+  };
+
+  if (typeof(obj) === 'object') {
+  	var stringifiedObj = '{';
+  	for (var k in obj) {
+  	  if (typeof(obj[k]) !== 'function' && obj[k] !== undefined) {
+  	  	if (k === Object.keys(obj)[Object.keys(obj).length - 1]) {
+	      stringifiedObj += stringifyJSON(k) + ':' + stringifyJSON(obj[k]);
+	    } else {
+	  	  stringifiedObj += stringifyJSON(k) + ':' + stringifyJSON(obj[k]) + ',';
+	    }	
+  	  }
+	}
+
+  	stringifiedObj += '}'
+  	return stringifiedObj;
+  }
+
 };
